91a23f31fff7a1443f023c687050d4c2812945ad
Steve Savitzky <steve@savitzky.net>
2016-03-28 22:01:07 -0700

    Pushed from nova Mon Mar 28 22:01:07 PDT 2016

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -1,7 +1,33 @@
 import XMonad
+
+import XMonad.Layout.Tabbed
+import           XMonad.Layout.Accordion
+
+import XMonad.Hooks.ManageDocks    -- dock/tray mgmt
+
+import qualified XMonad.Layout.Accordion
+import qualified XMonad.Layout.Circle
+import qualified XMonad.Layout.PerWorkspace
+import qualified XMonad.Layout.WorkspaceDir
  
-main = xmonad defaultConfig
+import XMonad.Hooks.UrgencyHook    -- window alert bells 
+import XMonad.Layout.Named         -- custom layout names
+import XMonad.Layout.NoBorders     -- smart borders on solo clients
+
+main = xmonad $ defaultConfig
         { modMask = mod4Mask -- Use Super instead of Alt
-        , terminal = "rxvt"
+        , terminal = "xterm"
+	, layoutHook = myLayoutHook
         -- more changes
         }
+
+-- | The available layouts.  Note that each layout is separated by |||, which
+-- denotes layout choice. 
+
+-- the default layout is fullscreen with smartborders applied to all
+myLayoutHook = smartBorders $ avoidStruts ( full ||| mtiled ||| tiled ||| Accordion )
+  where
+    full    = simpleTabbed
+    mtiled  = Mirror tiled
+    tiled   = Tall 1 (5/100) (2/(1+(toRational(sqrt(5)::Double))))
+    -- sets default tile as: Tall nmaster (delta) (golden ratio)
