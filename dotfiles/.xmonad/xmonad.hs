d488210f9ec755a76b026ca2d0b4926db89dcf0d
Steve Savitzky <steve@savitzky.net>
2016-05-17 22:10:31 -0700

    start dzen2 logs on all secondary screens.

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import System.Directory
 import System.IO                   -- hPutStrLn scope
 
 import XMonad.Actions.CycleWS 
+import XMonad.Actions.UpdatePointer
 
 import XMonad.Hooks.DynamicLog     -- statusbar
 import XMonad.Hooks.EwmhDesktops   -- extended window manager hints
@@ -26,7 +27,7 @@ import XMonad.Util.Run             -- spawnPipe and hPutStrLn
 import qualified XMonad.Layout.Circle
 import qualified XMonad.StackSet as W
 import qualified XMonad.Layout.WorkspaceDir
-
+import XMonad.Layout.IndependentScreens (countScreens)
 
 -- | Configuration options --
 --   These are the things you might want to tweak on different machines.
@@ -51,8 +52,12 @@ main = do
 
   haveGoodTerminal <- doesFileExist goodTerminal -- detect terminal emulator
   haveXmobar <- doesFileExist "/usr/bin/xmobar"  -- detect status bar program
+  mainbar <- spawnPipe $ myLogCommand haveXmobar -- spawn the status bar.
 
-  xmproc <- spawnPipe $ myLogCommand haveXmobar -- spawn the status bar.
+  -- All screens except the first get a dzen bar.  The first is generally smaller, and
+  -- in any case might have a gnome panel, system tray, etc.
+  nScreens <- countScreens 
+  dzens    <- mapM (spawnPipe .   dzenOnScreen) [2 .. nScreens] 
  
   xmonad $ ewmh $ defaultConfig
     { modMask = myModMask
@@ -60,7 +65,9 @@ main = do
     , layoutHook = myLayoutHook
     , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
     , workspaces = workspaceNames
-    , logHook = myLogHook haveXmobar xmproc
+    , logHook = updatePointer (0.5, 0.5) (0, 0) -- center cursor on newly focused window
+                >> myLogHook haveXmobar mainbar
+                >> mapM_ dzenLogHook dzens
     , handleEventHook = fullscreenEventHook -- makes fullscreen work properly
     }
     `additionalKeys` myAdditionalKeys workspaceNames
@@ -72,28 +79,30 @@ main = do
     ]
 
 
--- | The available layouts.  Note that each layout is separated by |||, which
--- denotes layout choice. 
-
--- the default layout is simpleTabbed with smartborders applied to all
--- see https://wiki.haskell.org/Xmonad/Config_archive/Thayer_Williams%27_xmonad.hs
--- for the golden ratio stuff and some other config things.
--- showWName $ looks awful with clickable workspace strings.  But we fixed that.
+-- | The available layouts.  Note that each layout in a parenthesized choice group is
+--   separated by |||, which denotes layout choice. 
+--
+--   see https://wiki.haskell.org/Xmonad/Config_archive/Thayer_Williams%27_xmonad.hs
+--   for the golden ratio stuff and some other config things.
 myLayoutHook = smartBorders $ avoidStruts $ showWName
-               $ onWorkspace "9" simpleFloat
-               $ onWorkspace "0" (Grid ||| spiral ||| Circle) -- 0 is a parking area, since xmonad doesn't use icons
-               $ ( full ||| tiled ||| mtiled ) -- applies to all workspaces not otherwise mentioned
+               -- 0 uses simpleFloat, so you don't have to add a doFloat to the manageHook
+               $ onWorkspace "0" float
+               -- - is a parking area, since xmonad doesn't use icons
+               $ onWorkspace "-" (Grid ||| spiral ||| Circle)
+               $ ( tabs ||| tall ||| wide ) -- applies to all workspaces not otherwise mentioned
   where
     phi     = (2/(1+(toRational(sqrt(5)::Double)))) -- Golden Ratio
-    full    = named "Tabs" simpleTabbed
-    mtiled  = named "Wide" $ Mirror tiled
-    tiled   = Tall 1 (2/100) phi     -- args to Tall:  nmaster delta golden ratio
+    tabs    = named "Tabs" simpleTabbed
+    tall    = Tall 1 (2/100) phi     -- args to Tall:  nmaster delta ratio
+    wide    = named "Wide" $ Mirror tall
     spiral  = named "Spiral" $ spiralWithDir East CW (6/7)
+    float   = named "Float" simpleFloat
 
 -- | The manage hook, which specifies how we treat particular kinds of windows.
---   Graphics programs tend to put up multiple windows, so they like a floating layout.    
+--   In addition to className one can use resourceName or title
 myManageHook = composeAll
-    [ className =? "Dia"            --> doFloat
+    [ -- doFloat for programs that put up lots of small windows.
+      className =? "Dia"            --> doFloat
     , className =? "Gimp"           --> doFloat
     , className =? "Inkscape"       --> doFloat
     , className =? "MPlayer"        --> doFloat
@@ -101,13 +110,13 @@ myManageHook = composeAll
     , className =? "XCalc"          --> doFloat
     , className =? "XClock"         --> doFloat
     , className =? "XSane"          --> doFloat
-      -- ignore panels, bars, and the like.
+      -- ignore panels, bars, and the like even if they don't set the appropriate WM hint
     , className =? "stalonetray"    --> doIgnore
     , className =? "panel"          --> doIgnore
     ]
 
--- | Select the status bar.  We use xmobar when it exists, but older versions
---   don't support the full configuration file format, so if we're stuck on Ubuntu 12.04
+-- | Select the status bar for the main screen.  We prefer xmobar, but older versions
+--   don't support the full configuration file format, so if we're stuck on Ubuntu < 15.04
 --   we use dzen2, which is drop-dead easy to recompile from source (see ../opt)
 myLogCommand mobar = if mobar then "xmobar" else dzenCommand
 myLogHook mobar    = if mobar then mobarLogHook else dzenLogHook
@@ -141,11 +150,13 @@ mobarLogHook pipe = dynamicLogWithPP xmobarPP
     }
   where clickWrap = if wsClickable then xmobarClickWrap else id
 
--- dzen2 log hook configuration.  Note that in order to have clickable
--- desktop names on older systems it may still be necessary to build
--- the latest version from source, but that should be simpler than xmobar.
-
+-- | dzen2 log hook configuration.  Note that in order to have clickable
+--   desktop names on older systems it may still be necessary to build
+--   the latest version from source, but that should be simpler than xmobar.
+--   Note that we use dzenOnScreen for all screens other than the first.
+--   The first screen is often a laptop, so it might be smaller.
 dzenCommand = "dzen2 -x '0' -y '0' -h '20' -w '1000' -ta 'l' -fg '#646464' -bg 'black' -fn '"++font++"'"
+dzenOnScreen n = dzenCommand ++ "-xs " ++ show n
 
 dzenClickWrap ws = wrap start end (dzenEscape ws)
   where start = "^ca(1,xdotool key super+" ++ [ head ws ] ++ ")"
@@ -160,7 +171,7 @@ dzenLogHook pipe = dynamicLogWithPP defaultPP
     , ppUrgent  = dzenColor "red" "yellow"             . clickWrap
     , ppTitle   = dzenColor "green"  "" . shorten 50                  --possibly 40 on laptops
     }
-                  where clickWrap = if wsClickable then dzenClickWrap else id
+  where clickWrap = if wsClickable then dzenClickWrap else id
 
 -- font and colors from xmobarrc
 font    =      "xft:Bitstream Vera Sans Mono:size=10:bold:antialias=true"
@@ -181,11 +192,11 @@ fgColor =      "#646464"
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 -- 
 workspaceNames = ["1","2","3","4","5","6","7","8","9" ] -- the predefined workspaces
-                 ++ [ "0" ]                             -- extra workspaces.
+                 ++ [ "0", "-" ]                             -- extra workspaces.
 
--- assign keys to the extra workspaces (only "0" so far)
+-- assign keys to the extra workspaces (only "0" and "-" so far)
 -- has to be done this way in order to pick up the clickable-wrapped actual names.
-wsKeys wsNames = zip [ xK_0 ] (drop 9 wsNames)
+wsKeys wsNames = zip [ xK_0, xK_minus ] (drop 9 wsNames)
 
 -- took me hours to figure out that modMask was bound to something unhelpful here
 -- You have to pass the fully-munged workspace names, which are only known after
