6ae4a006a8f8e8e908f90389ca3b842f19d68a55
Steve Savitzky <steve@savitzky.net>
2018-02-26 15:08:54 -0800

    start with minimal definition of to-do-mode

---
+++ emacs/to-do-mode.el
@@ -0,0 +1,16 @@
+;;; to-do-mode -- major mode for editing in to-do format
+;;;
+
+(provide 'to-do-mode)
+
+
+(defvar to-do-mode-hook nil
+  "Hook run when to-do-mode is loaded."
+  )
+
+(define-derived-mode to-do-mode text-mode "To-Do"
+  "Major mode for to-do files.
+\\{to-do-mode-map}
+Turning on To-Do mode runs the normal hook `to-do-mode-hook'."
+
+  )
