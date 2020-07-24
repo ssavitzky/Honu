2945045d251ea27d945526580177f2b23ddf1f08
Steve Savitzky <steve@savitzky.net>
2020-07-24 15:37:54 -0700

    Define to-do-mark-item-done (\C-C*)
    It's not really complete, but it does find the first "o" on the line and
    change it to an asterisk.  On a blank line, it simply inserts "  * ".
    Should be extended to handle lines with multiple items.

--- emacs/to-do-mode.el
+++ emacs/to-do-mode.el
@@ -12,6 +12,22 @@
   "Major mode for to-do files.
 \\{to-do-mode-map}
 Turning on To-Do mode runs the normal hook `to-do-mode-hook'."
-
+  
   (setq-local indent-tabs-mode nil)
+  (local-set-key "\C-C*" 'to-do-mark-item-done)
   )
+
+;;; to-do-mark-item-done 
+(defun to-do-mark-item-done ()
+  "Change the flag character to an asterisk." ; ought to go backward with C-U
+  (interactive nil)
+  (move-beginning-of-line 1)
+  (cond ((looking-at ".*  o ")		; this doesn't handle partial items.  Should search.
+	 (re-search-forward "  o ")
+	 (delete-backward-char 2)
+	 (insert "* "))
+	((looking-at "[[:space:]]*$")
+	 (insert "  *"))	; probably ought to handle indent.  tricky.
+	)
+  )
+
